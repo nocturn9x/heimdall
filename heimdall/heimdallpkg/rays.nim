3265b92415088b856a30f0c51e1234bd97ce9b25
nocturn9x <nocturn9x@nocturn9x.space>
2024-08-02 12:42:19 +0200

    Resolve cherry-pick conflicts

--- heimdall/heimdallpkg/rays.nim
+++ heimdall/heimdallpkg/rays.nim
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import bitboards
-import magics
-import pieces
+import heimdallpkg/bitboards
+import heimdallpkg/magics
+import heimdallpkg/pieces
 
 export bitboards, pieces
 
