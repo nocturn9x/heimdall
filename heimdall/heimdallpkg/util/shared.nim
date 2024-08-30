f84492c2adef0a735db78660449b0c8c28e357da
nocturn9x <nocturn9x@nocturn9x.space>
2024-08-30 13:47:36 +0200

    Refactor storage of search statistics and search state, improve modularity, fix time checks (bench 2201879)

---
+++ heimdall/heimdallpkg/util/shared.nim
@@ -0,0 +1,84 @@
+# Copyright 2024 Mattia Giambirtone & All Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## Shared stuff that can go across threads (somewhat) safely
+import std/atomics
+import std/monotimes
+
+
+import heimdallpkg/eval
+import heimdallpkg/moves
+import heimdallpkg/pieces
+
+
+type
+    SearchStatistics* = ref object
+        # The total number of nodes
+        # explored
+
+        nodeCount*: Atomic[uint64]
+        # The highest depth we explored to, including extensions
+        selectiveDepth*: Atomic[int]
+        # The highest fully cleared depth
+        highestDepth*: Atomic[int]
+        # The current principal variation being
+        # explored
+        currentVariation*: Atomic[int]
+        # The best score we found at root
+        bestRootScore*: Atomic[Score]
+        # The current best move
+        bestMove*: Atomic[Move]
+    
+    # Note: stuff that is not wrapped in an atomic is *not*
+    # meant to be used outside of the search manager. Proceed
+    # at your own risk
+
+    SearchState* = ref object
+        # Atomic booleans to control/inspect
+        # the state of the search
+        searching*: Atomic[bool]
+        stop*: Atomic[bool]
+        pondering*: Atomic[bool]
+        # When was the search started?
+        searchStart*: Atomic[MonoTime]
+        # Are we playing chess960?
+        chess960*: Atomic[bool]
+
+        # All static evaluations
+        # for every ply of the search
+        evals* {.align(64).}: array[255, Score]
+        # List of moves made for each ply
+        moves* {.align(64).}: array[255, Move]
+        # List of pieces that moved for each
+        # ply
+        movedPieces* {.align(64).}: array[255, Piece]
+        # The set of principal variations for each ply
+        # of the search. We keep one extra entry so we
+        # don't need any special casing inside the search
+        # function when constructing pv lines
+        pvMoves* {.align(64).}: array[255 + 1, array[255 + 1, Move]]
+        # The persistent evaluation state needed
+        # for NNUE
+        evalState*: EvalState
+
+
+proc newSearchState*: SearchState =
+    new(result)
+    for i in 0..255:
+        for j in 0..255:
+            result.pvMoves[i][j] = nullMove()
+
+
+proc newSearchStatistics*: SearchStatistics =
+    new(result)
