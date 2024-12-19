3a5a7852f61a32abc4859a9f88c8333698b492ff
nocturn9x <nocturn9x@nocturn9x.space>
2024-12-19 02:09:30 +0100

    Bucket the TT and get rid of old debugging code (bench 5795077)

--- heimdall/heimdallpkg/search.nim
+++ heimdall/heimdallpkg/search.nim
@@ -1014,9 +1014,9 @@ proc search(self: var SearchManager, depth, ply: int, alpha, beta: Score, isPV: 
         # Find the best move for us (worst move
         # for our opponent, hence the negative sign)
         var score: Score
-        # Prefetch next TT entry: 0 means read, 3 means the value has high temporal locality
+        # Prefetch next TT bucket: 0 means read, 3 means the value has high temporal locality
         # and should be kept in all possible cache levels if possible
-        prefetch(addr self.transpositionTable.data[getIndex(self.transpositionTable[], self.board.zobristKey)], cint(0), cint(3))
+        prefetch(addr self.transpositionTable.buckets[getIndex(self.transpositionTable[], self.board.zobristKey)], cint(0), cint(3))
         # Implementation of Principal Variation Search (PVS)
         if i == 0:
             # Due to our move ordering scheme, the first move is always the "best", so
