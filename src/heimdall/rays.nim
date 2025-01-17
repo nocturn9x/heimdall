1a09526c97d847002de40c7ddf45635cf7ce2778
nocturn9x <nocturn9x@nocturn9x.space>
2025-01-17 11:50:32 +0100

    Migrate package to Nim 2.2.0 (bench 5041843)

--- src/heimdall/rays.nim
+++ src/heimdall/rays.nim
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import heimdallpkg/bitboards
-import heimdallpkg/magics
-import heimdallpkg/pieces
+import heimdall/bitboards
+import heimdall/magics
+import heimdall/pieces
 
 export bitboards, pieces
 
