2028fea3eaa158c8a9e37d5e1a37ca7ff30a7d08
nocturn9x <nocturn9x@nocturn9x.space>
2025-07-20 16:37:14 +0200

    Initial work on SIMD multilayer inference (broken)

--- src/heimdall/util/simd.nim
+++ src/heimdall/util/simd.nim
@@ -23,6 +23,7 @@ when defined(avx512):
         VEPI32* = M512i
 
     const CHUNK_SIZE* = 32
+    const REGISTER_SIZE* = 512 div 8
 
     # Routines blatantly stolen from Alexandria. Many thanks cj!
     func vecZero16*: VEPI16 {.inline.} = mm512_setzero_si512()
@@ -33,22 +34,33 @@ when defined(avx512):
     func vecMax16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm512_max_epi16(vec0, vec1)
     func vecMin16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm512_min_epi16(vec0, vec1)
     func vecMullo16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm512_mullo_epi16(vec0, vec1)
+    func vecMulhi16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm512_mulhi_epi16(vec0, vec1)
     func vecMadd16*(vec0, vec1: VEPI16): VEPI32 {.inline.} = mm512_madd_epi16(vec0, vec1)
     func vecAdd16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm512_add_epi16(vec0, vec1)
     func vecAdd32*(vec0, vec1: VEPI32): VEPI32 {.inline.} = mm512_add_epi32(vec0, vec1)
     func vecSub16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm512_sub_epi16(vec0, vec1)
+    func vecLShift16*(vec: VEPI16, shift: int32 | uint32): VEPI16 {.inline.} = mm512_slli_epi16(vec, shift)
+    func vecRShift16*(vec: VEPI16, shift: int32 | uint32): VEPI16 {.inline.} = mm512_srli_epi16(vec, shift)
+    func vecRAShift16*(vec: VEPI16, shift: int32 | uint32): VEPI16 {.inline.} = mm512_srai_epi16(vec, shift)
+    func vecPackI16toU8*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm512_packus_epi16(vec0, vec1)
+    func vecPermute*(vec: VEPI16): VEPI16 {.inline.} = mm512_permutexvar_epi64(vec, mm512_setr_epi64(0, 2, 4, 6, 1, 3, 5, 7))
     func vecReduceAdd32*(vec: VEPI32): int32 {.inline.} = mm512_reduce_add_epi32(vec)
 else:
     when defined(avx2):
         {.localPassC:"-mavx2".}
 
         import nimsimd/avx2
+        import nimsimd/sse2
+
+        export MM_SHUFFLE
+
 
         type
             VEPI16* = M256i
             VEPI32* = M256i
 
         const CHUNK_SIZE* = 16
+        const REGISTER_SIZE* = 256 div 8
 
         func vecZero16*: VEPI16 {.inline.} = mm256_setzero_si256()
         func vecZero32*: VEPI32 {.inline.} = mm256_setzero_si256()
@@ -58,10 +70,16 @@ else:
         func vecMax16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm256_max_epi16(vec0, vec1)
         func vecMin16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm256_min_epi16(vec0, vec1)
         func vecMullo16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm256_mullo_epi16(vec0, vec1)
+        func vecMulhi16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm256_mulhi_epi16(vec0, vec1)
         func vecMadd16*(vec0, vec1: VEPI16): VEPI32 {.inline.} = mm256_madd_epi16(vec0, vec1)
         func vecAdd32*(vec0, vec1: VEPI32): VEPI32 {.inline.} = mm256_add_epi32(vec0, vec1)
         func vecAdd16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm256_add_epi16(vec0, vec1)
         func vecSub16*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm256_sub_epi16(vec0, vec1)
+        func vecLShift16*(vec: VEPI16, shift: int32 | uint32): VEPI16 {.inline.} = mm256_slli_epi16(vec, shift)
+        func vecRShift16*(vec: VEPI16, shift: int32 | uint32): VEPI16 {.inline.} = mm256_srli_epi16(vec, shift)
+        func vecRAShift16*(vec: VEPI16, shift: int32 | uint32): VEPI16 {.inline.} = mm256_srai_epi16(vec, shift)
+        func vecPackI16toU8*(vec0, vec1: VEPI16): VEPI16 {.inline.} = mm256_packus_epi16(vec0, vec1)
+        func vecPermute*(vec: VEPI16): VEPI16 {.inline.} = mm256_permute4x64_epi64(vec, MM_SHUFFLE(3, 1, 2, 0))
         # AVX2 doesn't have an intrinsic for vec_reduce_add_epi32 (AVX512 does), but thankfully
         # cj wrote the implementation for us!
         func vecReduceAdd32*(vec: VEPI32): int32 {.inline.} =
@@ -78,4 +96,7 @@ else:
 
             mm_cvtsi128_si32(sum32)
     else:
-        let CHUNK_SIZE* = 1
+        const CHUNK_SIZE* = 1
+        const REGISTER_SIZE* = 1
+
+    const I16_CHUNK_SIZE* = REGISTER_SIZE div sizeof(int16)
