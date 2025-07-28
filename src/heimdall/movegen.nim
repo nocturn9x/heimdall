e3d562b3f253bfe5cadfee7a3ce4b30ab7e425e8
nocturn9x <nocturn9x@nocturn9x.space>
2025-07-28 09:41:36 +0200

    Fix another wrong assertion in marlinformat test code. Retune WDL model + final release prep work (bench 7266885)

--- src/heimdall/movegen.nim
+++ src/heimdall/movegen.nim
@@ -837,7 +837,7 @@ proc basicTests* =
                 doAssert game.position.diagonalPins == newPos.diagonalPins, &"{game.position.diagonalPins} != {newPos.diagonalPins}"
                 doAssert game.position.zobristKey == newPos.zobristKey, &"{game.position.zobristKey} != {newPos.zobristKey}"
                 for sq in Square(0)..Square(63):
-                    doAssert game.position.mailbox[sq] != newPos.mailbox[sq], &"Mailbox mismatch at {sq}: {game.position.mailbox[sq]} != {newPos.mailbox[sq]}"
+                    doAssert game.position.mailbox[sq] == newPos.mailbox[sq], &"Mailbox mismatch at {sq}: {game.position.mailbox[sq]} != {newPos.mailbox[sq]}"
             except AssertionDefect:
                 echo &"Test failed for {fen} -> {board.toFEN()}"
                 raise getCurrentException()
